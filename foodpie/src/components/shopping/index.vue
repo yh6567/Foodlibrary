32dea447cfcedc29523b4b189555f36dc20225f4
yuanhao <381710738@qq.com>
2019-01-10 11:01:34 +0800

    袁灏修改的index和page

--- foodpie/src/components/shopping/index.vue
+++ foodpie/src/components/shopping/index.vue
@@ -1,50 +1,28 @@
 <template>
     <div>
-<<<<<<< HEAD
-       	<cate-com/>
-       	<evaluating-com/>
-
-       	<!-- <Page-com/> -->
-       	<!-- <Kap-com/> -->
-=======
     	
     <cate-com/>
    	<component :is="comName"></component>
      <!--  	<cate-com/>
        	<Page-com/>
        	<Kap-com/>-->
->>>>>>> 4c0aec3ea54b74c9ba82c7e56da597665da39d42
     </div>
 </template>
 
 <script>
 import Cate from "./cate/cate.vue";
-<<<<<<< HEAD
-// import Page from "./page/index.vue";
-import Home from './evaluating/evaluating.vue';
-import Kap from "./Kap/Kap.vue";
-=======
 import Page from "./page/index.vue";
 import Home from './evaluating/evaluating.vue';
-import Kap from './KAP/Kap.vue'
->>>>>>> 4c0aec3ea54b74c9ba82c7e56da597665da39d42
+import Kap from './kap/Kap.vue'
 export default {
   components: {
     "cate-com":Cate,
-<<<<<<< HEAD
-		// "Page-com":Page
-		// 'Kap-com':Kap
-		"evaluating-com":Home
-  }
-
-=======
     "Page-com":Page,
     'Home-com':Home,
 	'Kap-com':Kap
   },
   created(){
  	this.observer.$on('handle',(data)=>{
-   		console.log(data,'222')
  		this.index=data;
  		this.handleToggle(this.index)
  	})
@@ -62,24 +40,30 @@ export default {
 //				this.comName = name;
 	  methods:{
 	  	handleToggle(index) {
-	      switch (index){
-	      	 case 0:
-	          this.comName = "cate-com";
-	          break;
-	        case 1:
-	          this.comName = "Page-com";
-	          break;
-	        case 2:
-	          this.comName = "Home-com";
-	          break;
-	        case 3:
-	          this.comName = "Kap-com";
-	          break;
-//		      next();
-           }
+//	      switch (index){
+//	      	 case 0:
+//	          this.comName = "cate-com";
+//	          break;
+//	        case 1:
+//	          this.comName = "Page-com";
+//	          break;
+//	        case 2:
+//	          this.comName = "Home-com";
+//	          break;
+//	        case 3:
+//	          this.comName = "Kap-com";
+//	          break;
+				if(index==1){
+					this.comName = "cate-com";
+				}else if(index==2){
+					this.comName = "Home-com";
+				}else if(index==3){
+					this.comName = "Kap-com";
+				}else{
+        	   		this.comName = "Page-com";
+        	   }
          }
 	  }
->>>>>>> 4c0aec3ea54b74c9ba82c7e56da597665da39d42
 }
 </script>
 
