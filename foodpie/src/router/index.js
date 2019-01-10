d189f96296267b20fa4a2cd40dfbc8ca9375ae43
miowmiow <820750167@qq.com>
2019-01-10 08:48:51 +0800

    修正冲突

--- foodpie/src/router/index.js
+++ foodpie/src/router/index.js
@@ -5,15 +5,7 @@ import Router from 'vue-router'
  import shopping from './shopping'
  import my from "./my"
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-   import search from './search'
-=======
-//import search from './search'
->>>>>>> yh
-=======
 import search from './search'
->>>>>>> miowmiow
 
 Vue.use(Router)
   //底部导航有三个 划分了三个大块；我负责的baike下的路由自己参照我的配；不会可以问我
