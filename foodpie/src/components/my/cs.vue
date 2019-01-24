ef0e15cf022b74e1b64a038b9741109e78c6fd6d
jaychou1314 <986365404@qq.com>
2019-01-24 10:59:32 +0800

    周-最新

---
+++ foodpie/src/components/my/cs.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="cs">
+		<h2>测试Datetime Picker</h2>
+		<template>
+			<mt-datetime-picker ref="picker" type="time" v-model="pickerValue">
+			</mt-datetime-picker>
+			<mt-datetime-picker
+  v-model="pickerVisible"
+  type="date"
+  year-format="{value} 年"
+  month-format="{value} 月"
+  date-format="{value} 日">
+</mt-datetime-picker>
+		</template>
+	</div>
+</template>
+
+<script>
+	export default {
+		methods: {
+			openPicker() {
+				this.$refs.picker.open();
+			}
+		}
+	}
+</script>
+
+<style>
+	.cs {
+		width: 7.5rem;
+		height: 13.34rem;
+		background: #ccc;
+	}
+</style>
---
+++ foodpie/src/components/my/myUpload/uploadFood.vue
@@ -0,0 +1,84 @@
+<template>
+	<div class="uploadFood">
+		<div class="foodTop">
+			<img src="../../../assets/img/icon-rili-1@2x.png" @click="back()" />
+			<h2>我上传的食物</h2>
+		</div>
+		<ul class="foodMain">
+			<li>
+				<img src="../../../assets/img/sw_tp@2x.png" />
+				<p>
+					<span>美味早餐</span><br />
+					<span class="food">等待审核</span>
+				</p>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default{
+		methods:{
+			back(){
+				this.$router.push({path:"/myUpload"})				
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.uploadFood{
+		width: 7.5rem;
+		height: 13.34rem;
+		background: #fff;
+		overflow: hidden;
+		font-family: "微软雅黑";
+		.foodTop{
+			width: 100%;
+			height: .88rem;
+			margin-top: .4rem;
+			display: flex;
+			align-items: center;
+			position: relative;
+			justify-content: center;
+			img{
+				position: absolute;
+				left: .32rem;
+			}
+			h2{
+				font-size:.34rem;
+				font-weight: normal;
+				color:rgba(17,17,17,1);
+			}
+		}
+		.foodMain{
+			width: 100%;
+			height: 100%;
+			background: #F7F7F7;
+			li{
+				width: 100%;
+				height: 1.24rem;
+				border-bottom: 1px solid rgba(240,240,240,1);
+				margin: 0 .32rem;
+				display: flex;
+				align-items: center;
+				img{
+					width: .92rem;
+					height: .92rem;
+					margin-right: .28rem;
+				}
+				p{
+					span:nth-child(1){
+						font-size:.26rem;
+						color:rgba(17,17,17,1);
+						font-weight: bold;
+					}
+					.food{
+						font-size: .18rem;
+						color:rgba(160,159,159,1);
+					}
+				}
+			}
+		}
+	}
+</style>
